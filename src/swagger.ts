091e6aec02616da6d9a69fe3354061cd711b479a
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2025-01-30 11:09:01 +0200

    feat: add Swagger setup for API documentation in swagger.ts

---
+++ src/swagger.ts
@@ -0,0 +1,27 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { Express } from 'express';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'pWallet API',
+      version: '1.0.0',
+      description: 'API documentation for pWallet system',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Local development server',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.ts'],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+export const setupSwagger = (app: Express) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
